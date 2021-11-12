e71a5a79b18180dd49eca1168905da2810f2227d
Atik Hasan <atikhasan801@gmail.com>
2021-11-12 23:30:35 +0600

    helmet delete fix

--- helmet-server.js
+++ helmet-server.js
@@ -58,8 +58,9 @@ const run = async () => {
     });
 
     // api for delete a helmet
-    app.delete("helmets/:id", async (req, res) => {
+    app.delete("/helmets/:id", async (req, res) => {
       const id = req.params.id;
+      console.log(id);
       const query = { _id: ObjectId(id) };
 
       const result = await helmetCollection.deleteOne(query);
